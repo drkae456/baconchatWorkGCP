582010dc62e73bb5e9e1de14169b044aec35ae28
Bryce Bacon <brycebacon@Bryces-Mac-mini.local>
2024-12-24 11:37:46 +1100

    update to yml file

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const path = require("path");
 const app = express();
 const port = 80;
 
-// Serve static files from the src/public directory
-app.use(express.static(path.join(__dirname, "src", "views"))); // Adjusted to serve from 'src/public'
+// Serve static files from the src/views directory
+app.use(express.static(path.join(__dirname, "src", "views")));
 
 // Route to serve index.html
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "src", "views", "index.html")); // Adjusted path
+  res.sendFile(path.join(__dirname, "src", "views", "index.html"));
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+// Start the server - Modified to listen on all interfaces
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running at http://0.0.0.0:${port}`);
 });
