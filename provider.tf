c28c9b7e4465d2eb5ea89722d46db2131674c3fa
drkae456 <90262599+drkae456@users.noreply.github.com>
2024-12-30 21:49:57 +1100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,7 +5,6 @@ provider "google" {
 
 # Create the project
 resource "google_project" "project" {
-  name    =   var.project_name
   project_id      = var.project_id
   billing_account = var.billing_account_id
 }
