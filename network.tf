cd378e028508595da9f71cdb04000ae43d299383
drkae456 <90262599+drkae456@users.noreply.github.com>
2024-12-31 13:31:01 +1100

    Update network.tf

--- network.tf
+++ network.tf
@@ -25,6 +25,7 @@ resource "google_compute_subnetwork" "subnet" {
   count         = try(data.google_compute_subnetwork.existing_subnet.id, null) == null ? 1 : 0
   name          = var.subnet_name
   ip_cidr_range = "10.0.0.0/24"
+  region        = var.region
   network       = try(data.google_compute_network.existing_vpc.id, google_compute_network.vpc[0].id)
   project       = var.project_id
 }
